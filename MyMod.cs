47483c0cfedc60d3d1c99e6fe36039a980ce3f26
ChirenOnHive <44252697+chirenonhive@users.noreply.github.com>
2023-12-19 09:02:22 -0500

    Add project files.

---
+++ MyMod.cs
@@ -0,0 +1,40 @@
+﻿using SonsSdk;
+using TheForest;
+using UnityEngine;
+
+namespace MyMod;
+
+public class MyMod : SonsMod
+{
+    public MyMod()
+    {
+        // Nothing here yet. We can initialize custom callbacks here.
+    }
+
+    protected override void OnInitializeMod()
+    {
+        // Do your early mod initialization which doesn't involve game or sdk references here
+        Config.Init();
+    }
+
+    protected override void OnSdkInitialized()
+    {
+        // Do your mod initialization which involves game or sdk references here
+        // This is for stuff like UI creation, event registration etc.
+        MyModUi.Create();
+        GlobalInput.RegisterKey(KeyCode.Z, CloneKelvin()); // Register the Z key and assign it to the CloneKelvin() Action.
+    }
+
+    protected override void OnGameStart()
+    {
+        // This function is executed when the player joins a game right before he gains control of the character.
+    }
+
+    public static Action CloneKelvin()
+    {
+        return () =>
+        {
+            DebugConsole.TryRunDynamicCommand("addcharacter", "robby 1", DebugConsole.Instance); // Spawn a clone of Robby (AKA Kelvin) using console command
+        };
+    }
+}
---
+++ MyModUi.cs
@@ -0,0 +1,11 @@
+namespace MyMod;
+
+using static SUI.SUI;
+
+public class MyModUi
+{
+    public static void Create()
+    {
+        // Ui creation code goes here
+    }
+}
